2e7a0c498708d37a2cd04037038fa921059aee15
sethika77777777 <117151437+sethika77777777@users.noreply.github.com>
2025-04-16 04:44:22 -0400

    settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Axvel v.1.0.1',
   botName: "AXEVEL - MD",
   botOwner: 'Sethika (Axvel)', // Your name
-  ownerNumber: '+15068029617', //Your number
+  ownerNumber: '15068029617', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "privet",
   description: "Some body trying to use this bot without sethika's permissiion it's guy is a GAY",
